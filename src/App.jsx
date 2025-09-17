30a094b7bf93134e626cbc3e8eed7450e41583c1
Priyanka Asthana <166942678+PriyankaAsthana@users.noreply.github.com>
2025-09-17 22:38:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Cocktails from './components/Cocktails'
 import About from './components/About'
 import Art from './components/Art'
 import Menu from './components/Menu'
-import Contact from './components/Contact'
+import Contact from './components/Contact' 
 
 // Register GSAP plugins
 gsap.registerPlugin(ScrollTrigger, SplitText)
